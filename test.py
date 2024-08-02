bc2de75f1fd4bdfe6d01abae6f683c888489f4b1
hatdog <apur.rosales.swu@phinmaed.com>
2024-08-02 17:28:30 +0800

    First Commit

---
+++ test.py
@@ -0,0 +1,17 @@
+print("Hello, World!")
+
+number_one = 10
+number_two = 13
+
+first_name = 'Nim'
+char_sample = 'n'
+
+people_age = {
+'Nim': 20,
+'Mary': 21,
+'Jay': 18
+}
+
+total = number_one + number_two 
+
+print ('The result is', total)
